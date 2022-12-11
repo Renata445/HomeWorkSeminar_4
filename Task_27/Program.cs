ba1756baaf51d89649f2b31dc6bf4804e0b30545
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-11 20:39:24 +0300

    Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿int f(int a)
+{
+    int sum = 0, r;
+    while(a != 0)
+    {
+        r = a%10;
+        a = a/10;
+        sum = sum + r;
+    }
+    return sum;
+}
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a));
+
+
+
+
+ 
+            
