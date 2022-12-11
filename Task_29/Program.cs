4cb556eb2dceca0815d0340028606d67a6b679e9
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-11 20:04:33 +0300

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите элементы массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
