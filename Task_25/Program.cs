9af633792fcc056ab66b6dc4073b80f84475c2af
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-11 19:51:13 +0300

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int f(int a, int b)
+{
+    int result = 1;
+    for(int i = 1; i<= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a,b));
